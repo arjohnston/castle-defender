9bd3a8b973dbf0ec7c630f0fa5b393655a2301bb
Andrew Johnston <arjohnston3@gmail.com>
2022-02-15 23:11:02 -0800

    Update project settings, add netcode

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -111,8 +111,8 @@ namespace StarterAssets
 					//Don't multiply mouse input by Time.deltaTime;
 					float deltaTimeMultiplier = IsCurrentDeviceMouse ? 1.0f : Time.deltaTime;
 					
-					_cinemachineTargetYaw += _input.look.x * deltaTimeMultiplier * 0.5f;
-					_cinemachineTargetPitch += _input.look.y * deltaTimeMultiplier * 0.5f;
+					_cinemachineTargetYaw += _input.look.x * deltaTimeMultiplier;
+					_cinemachineTargetPitch += _input.look.y * deltaTimeMultiplier;
 				}
 
 				// clamp our rotations so our values are limited 360 degrees
---
+++ Assets/Scripts/helpers/NetworkCommandLine.cs
@@ -0,0 +1,67 @@
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+/*
+    To open the game via command prompt:
+    - Build the project (ctrl-B)
+    - Navigate to the project in the command prompt
+    - Run the project: "Castle Defender.exe" -mlapi <protocol>
+        - Where protocol: host, server, client
+
+    Note: the host is both the server and client.
+*/
+
+public class NetworkCommandLine : MonoBehaviour
+{
+   private NetworkManager netManager;
+
+   void Start()
+   {
+       netManager = GetComponentInParent<NetworkManager>();
+
+       if (Application.isEditor) {
+           Debug.Log("Detected Unity Editor mode. Starting networking as Host.");
+           netManager.StartHost();
+           return;
+        };
+
+       var args = GetCommandlineArgs();
+
+       if (args.TryGetValue("-mlapi", out string mlapiValue))
+       {
+           switch (mlapiValue)
+           {
+               case "server":
+                   netManager.StartServer();
+                   break;
+               case "host":
+                   netManager.StartHost();
+                   break;
+               case "client":
+                   netManager.StartClient();
+                   break;
+           }
+       }
+   }
+
+   private Dictionary<string, string> GetCommandlineArgs()
+   {
+       Dictionary<string, string> argDictionary = new Dictionary<string, string>();
+
+       var args = System.Environment.GetCommandLineArgs();
+
+       for (int i = 0; i < args.Length; ++i)
+       {
+           var arg = args[i].ToLower();
+           if (arg.StartsWith("-"))
+           {
+               var value = i < args.Length - 1 ? args[i + 1].ToLower() : null;
+               value = (value?.StartsWith("-") ?? false) ? null : value;
+
+               argDictionary.Add(arg, value);
+           }
+       }
+       return argDictionary;
+   }
+}
