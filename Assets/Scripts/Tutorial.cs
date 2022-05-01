1cbbef82071b673274c532201057607d7071ce11
Andrew Johnston <arjohnston3@gmail.com>
2022-05-01 08:25:27 -0700

    Add tutorials

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using Utilities.Singletons;
+using UnityEngine.UI;
+
+public class Tutorial : Singleton<Tutorial> {
+    public Sprite[] gallery; 
+    public Image displayImage;
+    public Button nextImg;
+    public Button prevImg;
+    public int i = 0;
+
+    public void BtnNext () {
+        if(i + 1 < gallery.Length){
+            i++;
+        }
+    }
+
+    public void BtnPrev () {
+        if (i - 1 > 0){
+            i --;
+        }
+    }
+
+    void Update () {
+        displayImage.sprite = gallery[i];
+    }
+}
